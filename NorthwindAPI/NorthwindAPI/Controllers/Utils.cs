d1b2c76d0c8d829f52f530efb21126a02b23f984
nishman89 <53156243+nishman89@users.noreply.github.com>
2022-10-21 12:04:33 +0100

    Add entity to DTO methods

---
+++ NorthwindAPI/NorthwindAPI/Controllers/Utils.cs
@@ -0,0 +1,28 @@
+﻿using NorthwindAPI.Models;
+using NorthwindAPI.Models.DTO;
+
+namespace NorthwindAPI.Controllers
+{
+    public static class Utils
+    {
+        public static SupplierDTO SupplierToDTO(Supplier supplier) =>
+            new SupplierDTO
+            {
+                SupplierId = supplier.SupplierId,
+                CompanyName = supplier.CompanyName,
+                ContactTitle = supplier.ContactTitle,
+                Country = supplier.Country,
+                TotalProducts = supplier.Products.Count,
+                Products = supplier.Products.Select(x => ProductToDTO(x)).ToList()
+            };
+        public static ProductDTO ProductToDTO(Product product) =>
+            new ProductDTO
+            {
+                ProductId = product.ProductId,
+                ProductName = product.ProductName,
+                SupplierId = product.SupplierId,
+                CategoryId = product.CategoryId,
+                UnitPrice = product.UnitPrice
+            };
+    }
+}
